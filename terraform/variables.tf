8c239db7aed0a2ab0cee82b443e2038a3d5c444e
kanja <kan.tyw@hotmail.com>
2024-11-17 11:43:53 +0700

    fix invalid type of tf var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "vpc_ip_range" {
 
 variable "subnet_cidr_block" {
   description = "cidr block for subnet"
-  type        = map(list[string])
+  type        = map(list(string))
   default = {
     public  = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
     private = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
